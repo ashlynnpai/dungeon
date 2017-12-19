edee47dc9f27d9d3c372127b7fb3d3687ed1e650
ashlynnpai <ashlynnpai@gmail.com>
2017-12-18 22:34:32 -0500

    add avatar and other display options

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.size = 200;
+    this.size = 400;
     this.rowSize = 20;
     const startPoint = 20;
     let grids = Array(this.size).fill("S");
-    let spaces = [21, 22, 23, 24, 42, 43, 44, 62, 63, 64,
-                 25, 26, 27, 46, 47, 66, 67, 86, 87, 48, 49, 50];
+    let spaces = [21, 22, 23, 24, 42, 43, 44, 62, 63, 64, 25, 26, 27, 46, 47, 66, 67, 86, 87, 48, 49, 50, 29, 30, 69, 70, 89, 90, 71, 32, 33, 52, 53, 72, 73, 34, 35, 36, 55, 56, 75, 76, 57, 58, 78, 98, 118, 117, 116, 115, 137, 157, 177, 176, 175, 155, 154, 153, 133, 113];
     
     for (let i=0; i<grids.length; i++) {
       if(spaces.includes(i)) {
@@ -17,7 +16,10 @@ class Game extends React.Component {
 
     this.state = {
     squares: grids,
-    player_index: startPoint
+    player_index: startPoint,
+    health: 20,
+    level: 1,
+    weapon: "hands"
                  };
   }
   
@@ -66,6 +68,13 @@ class Game extends React.Component {
   render() {
     return (
       <div onKeyPress={(e) => this.onKeyPressed(e)}>
+        <div id="display">
+          <p>Health: {this.state.health}</p>
+          <p>Level: {this.state.level}</p>
+          <p>Weapon: {this.state.weapon}</p>
+        <img className="avatar" src="https://www.ashlynnpai.com/assets/Idle__000.png" />
+          
+        </div>
         <div id="board" className="flex-container" >      
           {this.state.squares.map((square,index) => 
            <div className={square + "color"}  id={"square" + index} key={index}>{index} {square}</div>)}
