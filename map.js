3828582f6ed9e29fef9fcd7d7c9fccc5713b8c38
ashlynnpai <ashlynnpai@gmail.com>
2018-01-22 14:00:36 -0500

    extract seed functions to helpers

--- map.js
+++ map.js
@@ -16,25 +16,34 @@ squares[0][0] = "P";
 squares[0][19] = "pet";
 squares[2][57] = "balrog";
 
-let fixtures = [[25, 30, 31, 32, 39], [20, 25, 30, 35], [25, 30, 32, 34, 53]];
-fixtures.forEach(function(level, i) {
-  level.forEach(function(item, j) {
-    squares[i][item] = "F";
+squares = seedFixtures(squares);
+
+function seedFixtures(squares) {
+  let fixtures = [[25, 30, 31, 32, 39], [20, 25, 30, 35], [25, 30, 32, 34, 53]];
+  fixtures.forEach(function(level, i) {
+    level.forEach(function(item, j) {
+      squares[i][item] = "F";
+    });
   });
-});
+  return squares;
+}
 
 //stairs "down" "up"
 //switch level in keypress
-let stairs = [{level:0, index:59, direction:"down"}, {level:1, index:0, direction:"up"},
-{level:1, index:59, direction:"down"}, {level:2, index:0, direction:"up"}];
 
-stairs.forEach(function(item) {
-  squares[item.level][item.index] = item.direction;
-});
+squares = seedStairs(squares);
 
+function seedStairs(squares) {
+  let stairs = [{level:0, index:59, direction:"down"}, {level:1, index:0, direction:"up"},
+  {level:1, index:59, direction:"down"}, {level:2, index:0, direction:"up"}];
+
+  stairs.forEach(function(item) {
+    squares[item.level][item.index] = item.direction;
+  });
+  return squares;
+}
 
 //random seed mobs, items
-
 
 
 
