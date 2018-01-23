ecb962d2b92deda278c16c3aa941e9ad2dded7b8
ashlynnpai <ashlynnpai@gmail.com>
2018-01-23 14:19:39 -0500

    attributions for flash icons

--- map.js
+++ map.js
@@ -130,6 +130,5 @@ processFoundItems(itemName) {
 
 
 //add toggle darkness
-//add extra levels
 //switch maplevel in keypress when char hits stairs
 //rewrite fixtures to put name of fixture and set background in css
